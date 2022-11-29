c99be7546c6a1831c95e52f317861b2427f28bab
HackLikeDemons <hello-github@wienes.com>
2022-11-29 22:22:16 +0100

    added mittens

---
+++ mittens_find_mits/find_mits.py
@@ -0,0 +1,15 @@
+solution_lines = []
+allowed_chars = ["\\","`","/","_","\n"," ",","]
+
+for i in range(1,7):
+    file_name = f"mits{i}.txt"
+    file = open(file_name, "r")
+    file_lines = file.readlines()
+    file.close()
+    for line in file_lines:
+        if "/" in line:
+            if "_" in line:                
+                    line = ''.join(c for c in line if c in allowed_chars)
+                    solution_lines.append(line)
+
+print("".join(solution_lines))
